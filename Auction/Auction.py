7645094344e7ba7e68aa60b1735fbe0e8d01abf9
Bao Truong <dinhbao10t4@gmail.com>
2018-03-28 18:41:49 +0700

    update

--- Auction/Auction.py
+++ Auction/Auction.py
@@ -15,12 +15,14 @@ class Item(db.Model):
     name = Column(String, nullable=False)
     description = Column(String, nullable=False)
     start_time = Column(DateTime)
+    user_id = Column(Integer, ForeignKey('user.id'))
 
-    def __init__(self, id, name, description, start_time):
+    def __init__(self, id, name, description, start_time, user_id):
         self.id = id
         self.name = name
         self.description = description
         self.start_time = start_time
+        self.user_id = user_id
 
 class User(db.Model):
     __tablename__ = 'user'
@@ -62,27 +64,31 @@ def hello_world():
     db.create_all()
     return 'init database!'
 
-@app.route('/create3User')
+@app.route('/create4User')
 def createUser():
     nam = User(1, 'nam', '123456')
     viet = User(2, 'viet', '123456')
     nu = User(3, 'nu', '123456')
+    boss = User(4, 'boss', '123456')
 
     db.session.add(nam)
     db.session.add(viet)
     db.session.add(nu)
+    db.session.add(boss)
 
     db.session.commit()
 
-    return 'create 3 user'
+    return 'create 4 user'
 
-@app.route('/createBaseball')
+@app.route('/createBaseballAndCar')
 def createBaseball():
-    baseball = Item(1, 'baseball', 'sport ball', '2009/05/31:12:00:00AM')
+    baseball = Item(1, 'baseball', 'sport ball', '2009/05/31:12:00:00AM', 4)
+    car = Item(2, 'car', 'sport car', '2009/05/31:12:00:00AM', 4)
     db.session.add(baseball)
+    db.session.add(car)
     db.session.commit()
 
-    return 'create item baseball'
+    return 'create item baseball & car'
 
 @app.route('/bid-data')
 def bidData():
@@ -111,12 +117,38 @@ def bidData():
     db.session.add(nu_baseball1)
     db.session.add(nu_baseball2)
 
+    #CAR
+    nam_car = UserItem(4, 1, 2)
+    viet_car = UserItem(5, 2, 2)
+    nu_car = UserItem(6, 3, 2)
+
+    nam_car1 = Bid(7, 10.5, 4)
+    nam_car2 = Bid(8, 20.5, 4)
+    viet_car1 = Bid(9, 9.5, 5)
+    viet_car2 = Bid(10, 13.5, 5)
+    nu_car1 = Bid(11, 12.5, 6)
+    nu_car2 = Bid(12, 15.5, 6)
+
+    # create user bid item
+    db.session.add(nam_car)
+    db.session.add(viet_car)
+    db.session.add(nu_car)
+    db.session.commit()
+
+    # bid price
+    db.session.add(nam_car1)
+    db.session.add(nam_car2)
+    db.session.add(viet_car1)
+    db.session.add(viet_car2)
+    db.session.add(nu_car1)
+    db.session.add(nu_car2)
+
     db.session.commit()
     return 'init some data bid'
 
 @app.route('/select')
 def select():
-    query_result = Bid.query.join(UserItem, Bid.user_item_id == UserItem.id).join(User, UserItem.user_id == User.id).add_columns(Bid.price, User.username, UserItem.item_id).filter(UserItem.item_id == 1).order_by(Bid.price.desc()).first()
+    query_result = UserItem.query.filter(UserItem.item_id == 1).join(Bid, Bid.user_item_id == UserItem.id).join(User, UserItem.user_id == User.id).add_columns(Bid.price, User.username, UserItem.item_id).order_by(Bid.price.desc()).first()
     return 'user placed the highest bid: ' + str(query_result.username) + ' with ' + str(query_result.price)
 
 if __name__ == '__main__':
